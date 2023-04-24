56dfe84708860828dd7debbe002751bf49d338b1
Shazid Hossain <bzshazid@gmail.com>
2023-04-24 22:45:10 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+"""
+In this exercise module we are going to learn about how we can make a calculr by using Python Programming such as: 
+
+"""
+
+# Taking the Numbers from the user:
+
+# Number: 01
+num1 = int(input("Enter the First Number:\n"))
+
+# Number: 02
+num2 = int(input("Enter the Second Number:\n"))
+
+# Printing the Numbers:
+print("You Have Given the Numbers as:" + " " + " ", num1,num2)
+
+# Calculating Part:
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+mod = num1 % num2
+floorDiv = num1 // num2
+power = num1**2
+
+print("Addition is: " , add)
+print("Subtraction is: " , sub)
+print("Multiplication is: " , mul)
+print("Division is: " , div)
+print("Power is: " , power)
+print("Modulas is: " , mod)
+print("Floor Division is: " , floorDiv)
